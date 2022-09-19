2c7341af1f0a085341edb03ab9504e595af7b7b0
gussti <gusaranciba@gmail.com>
2022-09-19 17:46:37 -0300

    nuevamente react-app creada

---
+++ react-redux-crud/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { useSelector } from 'react-redux'
+
+function App() {
+  const tasksState = useSelector(state => state.tasks)
+  console.log(tasksState)
+
+
+  return (
+    <div className="App">
+      <h1>Holasss</h1>
+    </div>
+  );
+}
+
+export default App;
