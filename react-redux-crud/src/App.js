303802746a0ae2b296b1925a7f2d3b3b1b25a70c
gussti <gusaranciba@gmail.com>
2022-09-19 18:59:56 -0300

    accediendo al estado usando RTK

--- react-redux-crud/src/App.js
+++ react-redux-crud/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import { useSelector } from 'react-redux'
+import TasksForm from './components/TasksForm';
+import TasksList from './components/TasksList';
 
 function App() {
   const tasksState = useSelector(state => state.tasks)
@@ -10,6 +12,9 @@ function App() {
   return (
     <div className="App">
       <h1>Holasss</h1>
+
+      <TasksForm/>
+      <TasksList/>
     </div>
   );
 }
